9ae5ea502e146a3a051ae376e3acac56e12548f4
Garyeong Bae <orisumin@gmail.com>
2026-06-25 13:38:08 -0600

    child classes

--- Goal.cs
+++ Goal.cs
@@ -1,22 +1,3 @@
 public class Goal
-{public void savefile()
-    {
-        string fileName = "goaltracker.txt";
-
-        using (StreamWriter outputFile = new StreamWriter ("goaltracker.txt"))
-        {
-            outputFile.WriteLine("Example.");
-            
-        }
-    }
-    public void ReadFile()
-    {
-        string fileName = "goaltracker.txt";
-        string[] lines = System.IO.File.ReadAllLines("goaltracker.txt");
-
-        foreach (string line in lines)
-        Console.Write(line);
-    }
-
-    
+{  
 }
--- Program.cs
+++ Program.cs
@@ -6,4 +6,24 @@ class Program
     {
         Console.WriteLine("Hello Develop05 World!");
     }
+    public void savefile()
+    {
+        string fileName = "goaltracker.txt";
+
+        using (StreamWriter outputFile = new StreamWriter ("goaltracker.txt"))
+        {
+            outputFile.WriteLine("Example.");
+            
+        }
+    }
+    public void ReadFile()
+    {
+        string fileName = "goaltracker.txt";
+        string[] lines = System.IO.File.ReadAllLines("goaltracker.txt");
+
+        foreach (string line in lines)
+        Console.Write(line);
+    }
+
+  
 }
