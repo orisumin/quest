49a7be8451bc45523c6e8d42821562c656fa695a
Jacob Bateman <bat18004@byui.edu>
2026-06-26 21:43:26 -0600

    updated Goal.cs

--- Goal.cs
+++ Goal.cs
@@ -1,3 +1,37 @@
-public class Goal
-{  
+public class Goal:Program
+{   protected string jb_name = protected string jbName;
+    protected string jb_description = protected string jbDescription;
+    protected string jb_point = protected int Point;    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    public void savefile()
+    {
+        string fileName = "goaltracker.txt";
+
+        using (StreamWriter outputFile = new StreamWriter ("goaltracker.txt"))
+        {
+            outputFile.WriteLine("Example.");
+            
+        }
+    }
+    
+    public void ReadFile()
+    {
+        string fileName = "goaltracker.txt";
+        string[] lines = System.IO.File.ReadAllLines("goaltracker.txt");
+
+        foreach (string line in lines)
+        Console.Write(line);
+    }
+    
+
+    
 }
